ea8419cfc399dceb1836b90bb9f80bfd38600fae
bTKHS21 <46063092+bTKHS21@users.noreply.github.com>
2019-01-09 09:16:37 +0900

    add

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-let bool_01 = true;
-
-let app = new Vue({
-    el: "#app",
-    data: {
-        msg: "welcome",
-        msg01:""
-    },
-    methods: {
-        change_function: () => {
-            if (bool_01) {
-                app.msg01 = "Hello"
-                bool_01 = false;
-            } else {
-                app.msg01 = ""
-                bool_01 = true;
-            }
-        }
+const a_01 = document.getElementById("id_04");
+const a_02 = document.getElementById("id_02");
+let a_03 = true;
+a_01.addEventListener("click", e => {
+    if (a_03) {
+        a_02.innerText = "Hello";
+        a_03 = false;
+    } else {
+        a_02.innerText = "";
+        a_03 = true;
     }
-})
+});
